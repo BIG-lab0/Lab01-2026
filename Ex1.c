6f1c329a3ed6fa5c396d681111fe10eb494d59a2
PEDRO NICOLETTI GALANTE <nicolettipedrogalante@gmail.com>
2026-02-11 20:09:15 -0300

    Implement salary calculation in Ex1.c

---
+++ Ex1.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+
+int salario_minimo = printf("Digite o valor do seu salário minímo"); 
+int salario_bruto = printf("Digite o valor do seu salário bruto");
+int salario_final = salario_bruto / salario_minimo;
+
+printf(salario_final);
+
+return 0 
